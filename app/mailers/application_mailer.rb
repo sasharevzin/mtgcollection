d5b8b03d8c7a63c20ea1c00e176464affdbe04bc
Donavin Northrup <dnorthrup25@gmail.com>
2016-08-23 22:42:40 +0000

    Add account activation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'mikecheck-dnorthrup.c9users.io'
   layout 'mailer'
 end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -7,16 +7,20 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @non_admin = users(:archer)
   end
 
-  test "index as admin including pagination and delete links" do
+  test "index as admin with activated users pagination and delete links" do
     log_in_as(@admin)
     get users_path
     assert_template 'users/index'
-    assert_select 'div.pagination'
+    assert_select 'div.pagination', count: 2
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      if user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+      else
+        assert_select 'a[href=?]', user_path(user), false
       end
     end
     assert_difference 'User.count', -1 do
@@ -29,4 +33,4 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_select 'a', text: 'delete', count: 0
   end
-end
+end 
